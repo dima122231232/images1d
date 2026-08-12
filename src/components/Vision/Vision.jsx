ce14d374c5f6b39899ee2850c68b37272081c1ae
Dmytro <dimokmilok10@gmail.com>
2026-08-12 20:50:31 +0200

    first push

---
+++ src/components/Vision/Vision.jsx
@@ -0,0 +1,22 @@
+import "./Vision.css";
+
+export default function Vision() {
+  return (
+    <>
+        <section className="vision">
+            <div className="container">
+                <div>
+                    <div>
+                        <h2>The next century of innovation 
+will be the design of better systems.</h2>
+                        <p>If Imagination is our collective capacity to articulate better futures. Design is the method to rigorously bring them to fruition by building new models.</p>
+                    </div>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </section>
+    </>
+  );
+}
